1cde7500a47528a89c19ebb59f45f96c5c8a1c91
Reem <202110769@ua.edu.lb>
2024-04-28 21:02:44 +0300

    add buyer part

--- app/Models/Store.php
+++ app/Models/Store.php
@@ -8,12 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Store extends Model
 {
     use HasFactory;
-    public function getProduct(){
-        return $this->hasMany(Product::class);
+    public function getUsers(){
+        return $this->belongsTo(User::class,'user_id','id');
+
     }
 
-    public function getUser(){
-        return $this->belongsTo(UserAuth::class,'user_id','id');
+    public function follows()
+    {
+        return $this->belongsToMany(User::class, 'follows', 'store_id', 'buyer_id');
+    }
+
+    public function getCategories(){
+        return $this->hasMany(Category::class);
+    }
+    
+    public function products(){
+        return $this->hasMany(Product::class);
+    }
+     public function getItem(){
+        return $this->hasOne(ShoppingCart::class);
     }
 
 }

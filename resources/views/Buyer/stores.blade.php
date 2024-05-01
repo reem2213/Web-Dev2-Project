b006d081b5de1c108f476c631e300b5ce1f28abf
Reem <202110769@ua.edu.lb>
2024-05-01 16:56:11 +0300

    update buyer part

--- resources/views/Buyer/stores.blade.php
+++ resources/views/Buyer/stores.blade.php
@@ -335,7 +335,6 @@
                 </div>
             </div>
 
-
             <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
             <script>
                 $(document).ready(function() {

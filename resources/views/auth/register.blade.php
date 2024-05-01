7b6f2e73c57b3eba05959a21fee9ce9f3df250cc
Reem <202110769@ua.edu.lb>
2024-05-02 01:52:10 +0300

    update user auth frontend

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,19 +5,25 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Create') }}
-                    <div class="card-header2">{{ __('an account') }}</div>
+                <div class="p1">
+                    <div class="card-header">{{ __('Create') }}</div>
+                    <div class="welcome-message">
+                        <img src="{{asset('storage/images/login.png')}}" alt="Welcome Image" class="welcome-image">
+                    </div>
                 </div>
+
+                <div class="card-header2">{{ __('an account') }}</div>
+
 
 
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
+                    <form method="POST" action="{{ route('register') }}" class="form">
                         @csrf
 
                         <div class="form-group">
                             <label class="form-label" for="username">{{ __('Username') }}</label>
-                            <input id="username" type="numeric" class="form-control custom-input @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" autocomplete="username" autofocus>
+                            <input placeholder="enter a username" id="username" type="numeric" class="form-control custom-input @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" autocomplete="username" autofocus>
                             @error('username')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
@@ -27,7 +33,7 @@
 
                         <div class="form-group">
                             <label class="form-label" for="email">{{ __('Email Address') }}</label>
-                            <input id="email" class="form-control custom-input @error('email') is-invalid @enderror" name="email" type="numeric" value="{{ old('email') }}" autocomplete="email">
+                            <input placeholder="enter an email" id="email" class="form-control custom-input @error('email') is-invalid @enderror" name="email" type="numeric" value="{{ old('email') }}" autocomplete="email">
                             @error('email')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
@@ -37,7 +43,7 @@
 
                         <div class="form-group">
                             <label class="form-label" for="password">{{ __('Password') }}</label>
-                            <input id="password" type="password" class="form-control custom-input @error('password') is-invalid @enderror" name="password" autocomplete="new-password">
+                            <input placeholder="enter a password" id="password" type="password" class="form-control custom-input @error('password') is-invalid @enderror" name="password" autocomplete="new-password">
                             @error('password')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
@@ -48,7 +54,7 @@
                         <div class="form-group">
                             <label class="form-label" for="password-confirm">{{ __('Confirm Password') }}</label>
 
-                            <input id="password-confirm" type="password" class="form-control custom-input" name="password_confirmation" autocomplete="new-password">
+                            <input placeholder="confirm your password" id="password-confirm" type="password" class="form-control custom-input" name="password_confirmation" autocomplete="new-password">
                             @error('password')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
@@ -58,7 +64,7 @@
 
                         <div class="form-group">
                             <label class="form-label" for="phoneNo">{{ __('Phone Number') }}</label>
-                            <input id="phoneNo" type="numeric" class="form-control custom-input @error('phoneNo') is-invalid @enderror" name="phoneNo" value="{{ old('phoneNo') }}" autocomplete="phoneNo" autofocus>
+                            <input placeholder="enter a phone no." id="phoneNo" type="numeric" class="form-control custom-input @error('phoneNo') is-invalid @enderror" name="phoneNo" value="{{ old('phoneNo') }}" autocomplete="phoneNo" autofocus>
                             @error('phoneNo')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
@@ -98,9 +104,13 @@
                             </button>
                         </a>
                     </div>
-                    <div class="welcome-message">
-                        <img src="{{asset('storage/images/login.png')}}" alt="Welcome Image" class="welcome-image">
-                    </div>
+                    <p class="btn btn-signup">Already have an account?
+
+                        <a class="signup" href="{{ route('login') }}">
+                            {{ __('Login') }}
+                        </a>
+                    </p>
+
                 </div>
             </div>
         </div>

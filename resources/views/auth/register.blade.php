1ab8345d50d99a08f811cc59f2ccf93806782f76
Reem <202110769@ua.edu.lb>
2024-05-08 03:36:50 +0300

    updates

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -82,6 +82,13 @@
                     <p class="or">Or Sign In With</p>
 
                     <div class="w-full flex flex-container flex-row absolute">
+                        <a href="/auth/facebook/redirect" type="button" class="social-media">
+                            <button type="button" class="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 me-2 mb-2">
+                                <svg class="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
+                                    <path fill-rule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clip-rule="evenodd" />
+                                </svg>
+                            </button>
+                        </a>
                         <a href="/auth/github/redirect" type="button" class="social-media">
                             <button type="button" class="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 me-2 mb-2">
                                 <svg class="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
